98ce8227e68dee62091572d4de558b839046fbf0
zetvyy <zetvyy@gmail.com>
2024-07-30 19:23:03 +0700

    handle cors header

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(logger("dev"));
 app.use(flash());
 app.use(express.json());
