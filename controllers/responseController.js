1c8f50aa77db9dbed8e8b437c2f8d2992c9384fc
zetvyy <zetvyy@gmail.com>
2024-08-13 16:47:22 +0700

    get data tes with email

--- controllers/responseController.js
+++ controllers/responseController.js
@@ -19,12 +19,31 @@ module.exports = {
   getDataTest: async (req, res) => {
     let client;
     try {
+      const email = req.headers["x-user-email"];
+
+      if (!email) {
+        return response(400, null, "Email tidak diberikan", res);
+      }
       client = await MongoClient.connect(mongoURL);
       const db = client.db(dbName);
       const collection = db.collection("response"); // Ganti dengan nama koleksi Anda
 
-      const formResponse = await collection.find().toArray();
-      response(200, formResponse, "Get Form Response Tes Minat berhasil", res);
+      const formResponse = await collection.findOne({ email: email });
+      if (formResponse.length === 0) {
+        return response(
+          404,
+          null,
+          "Data tidak ditemukan untuk email tersebut",
+          res
+        );
+      } else {
+        response(
+          200,
+          formResponse,
+          "Get Form Response Tes Minat berhasil",
+          res
+        );
+      }
     } catch (error) {
       console.error("Error in getDataTest:", error);
       response(500, null, "Terjadi kesalahan saat mengambil data", res);
